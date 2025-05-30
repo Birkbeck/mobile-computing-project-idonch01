20e83896d8786adc10ae4c7db1a34637294938cb
idonch01 <148148659+idonch01@users.noreply.github.com>
2025-05-30 13:29:13 +0100

    Add files via upload

---
+++ app/src/main/java/co/uk/bbk/culinarycompanion/RecipeDetailActivity.java
@@ -0,0 +1,13 @@
+
+package co.uk.bbk.culinarycompanion;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RecipeDetailActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_detail);
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "CulinaryCompanion"
+include(":app")
